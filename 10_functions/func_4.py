f043a8e113c87203b26dd91f2af0bb45f4e5007e
Swyam Shukla <swyamshukla0104@gmail.com>
2026-09-16 16:16:45 +0530

    functions lamda

---
+++ 10_functions/func_4.py
@@ -0,0 +1,8 @@
+
+
+def greet(name='swyam'):
+    print(f"Hello, {name}")
+
+
+greet("shivam")
+greet()
---
+++ 10_functions/func_5_lamda.py
@@ -0,0 +1,18 @@
+
+cube = lambda x: x**3;
+
+print(cube(3))
+
+
+
+
+
+
+
+
+def printSum(*args):
+    for i in args:
+        print(i)
+
+
+print(printSum(23,4,5,4,2));
