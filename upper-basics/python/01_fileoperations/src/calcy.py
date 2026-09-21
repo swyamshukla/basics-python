5b1f671a1436a8305cc34aa77f839c326772c72a
Swyam Shukla <swyamshukla0104@gmail.com>
2026-09-21 21:26:53 +0530

    learning

---
+++ upper-basics/python/01_fileoperations/src/calcy.py
@@ -0,0 +1,23 @@
+from math import add,sub,mul,div
+
+
+print(add(10,20))
+
+print(sub(10,20))
+
+print(mul(10,20))
+
+print(div(10,20))
+
+
+
+
+# import math
+
+# print(math.add(10,20))
+
+# print(math.sub(10,20))
+
+# print(math.mul(10,20))
+
+# print(math.div(10,20))
